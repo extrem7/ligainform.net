95c907d70a1cd1e8b7bbb4da5f4c698de9b8aa47
Олександр Заставний <alex.roox5@gmail.com>
2019-11-04 15:05:28 +0200

    Change css and in item-small blade postition date

--- views/articles/includes/item-small.blade.php
+++ views/articles/includes/item-small.blade.php
@@ -1,4 +1,4 @@
 <div class="sidebar-article-item">
-    <div class="article-time text-center">{{time_diff()}}</div>
     <a href="@link" class="article-title text-center title-line-cap">@title</a>
+    <div class="article-time text-center">{{time_diff()}}</div>
 </div>
