96cc4af0b7aae433775e52d20fe07e9d5787d7a8
Олександр Заставний <alex.roox5@gmail.com>
2020-02-13 11:50:12 +0200

    Rss change

--- views/feed.php
+++ views/feed.php
@@ -114,8 +114,8 @@ do_action( 'rss_tag_pre', 'rss2' );
                         <content:encoded><![CDATA[<?php the_excerpt_rss(); ?>]]></content:encoded>
                     <?php endif; ?>
                 <?php endif; ?>
-                <?php if (the_category_rss('rss2') == '	Эксклюзив'): ?>
-                    <yandex:full-text><?php get_the_content_feed('rss2'); ?></yandex:full-text>
+                <?php $cat = get_the_category_rss('rss2'); if ($cat == "Эксклюзив"): ?>
+                    <yandex:full-text><?php echo get_the_content_feed('rss2'); ?></yandex:full-text>
                 <? endif; ?>
                 <?php if ( get_comments_number() || comments_open() ) : ?>
                     <wfw:commentRss><?php echo esc_url( get_post_comments_feed_link(null, 'rss2') ); ?></wfw:commentRss>
